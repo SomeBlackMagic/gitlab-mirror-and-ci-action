e19529a88eb13b2983d96b3fc80ad9d199817baf
Andru Cherny <wiroatom@gmail.com>
2023-01-28 14:25:39 +0200

    .

--- cmd.sh
+++ cmd.sh
@@ -31,6 +31,7 @@ branch="$(git symbolic-ref --short HEAD)"
 
 branch_uri="$(urlencode ${branch})"
 
+sh -c "git config --global --add safe.directory /github/workspace"
 sh -c "git config --global credential.username ${GITLAB_USERNAME}"
 sh -c "git config --global core.askPass /cred-helper.sh"
 sh -c "git config --global credential.helper cache"
